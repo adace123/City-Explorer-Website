4a44f4a474012eb22eb3a1f819bc040586784d6e
adace123 <rexforddrive@gmail.com>
2016-12-08 14:34:39 -0800

    Update capitalData.php

--- capitalData.php
+++ capitalData.php
@@ -6,19 +6,22 @@ if ($content_type_args[0] == 'application/json')
 header("Cache-Control: no-cache, no-store, must-revalidate");
 var_dump($_POST);
 
+//variables to hold input fields
 $city = $_POST['city'];
 $country = $_POST['country'];
 $continent = $_POST['continent'];
 $image = $_POST['image'];
 $state = $_POST['state'];
 
+//store inputs in array
 $object_data = array(
 'city' => $city,
 'country' => $country,
 'continent' => $continent,
 'image' => $image,
 'state' => $state
-);	
+);
+//write inputs to JSON database
 print_r($object_data);
 $capitals = file_get_contents('capitals.json');
 $capital_data = json_decode($capitals);	
