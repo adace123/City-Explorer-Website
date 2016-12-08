f188dcbae85e2e8900a9185b472b33d364f5f19f
adace123 <rexforddrive@gmail.com>
2016-12-08 14:44:45 -0800

    Update login.php

--- login.php
+++ login.php
@@ -3,7 +3,7 @@
 $content_type_args = explode(';', $_SERVER['CONTENT_TYPE']); 
 if ($content_type_args[0] == 'application/json') 
   $_POST = json_decode(file_get_contents('php://input'),true);
-
+//checks if username and password are correct
 $user = $_POST["username"];
 $pass = $_POST["password"];
 if($user == "cityuser" && $pass == "citypass"){
